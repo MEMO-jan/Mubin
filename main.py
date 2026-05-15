698cf0b9a9cca49b1c936119f9cc3c415a8ca994
MEMO-jan <mubinsafa33@gmail.com>
2026-05-15 23:31:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-TOKEN = "MTUwNDc5NzA5NTA5MjAyNzQ5Mw.GRBq9k.uWXseBr4IBf90X52IZws3WgurEU8YdFQoYvITE"
+TOKEN = "MTUwNDc5NzA5NTA5MjAyNzQ5Mw.GZuZqU.6oofnE6XMeHCYOBJl-oyMLIEuv-dotFaYrBUS8"
 
 intents = discord.Intents.default()
 intents.message_content = True
