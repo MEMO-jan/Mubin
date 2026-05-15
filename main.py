8948bc76b9035f158eb6b0b575258eca2b8b4760
MEMO-jan <mubinsafa33@gmail.com>
2026-05-15 23:35:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 import discord
 from discord.ext import commands
 
-TOKEN = "MTUwNDc5NzA5NTA5MjAyNzQ5Mw.GZuZqU.6oofnE6XMeHCYOBJl-oyMLIEuv-dotFaYrBUS8"
+# Token is NOT stored here
+TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
